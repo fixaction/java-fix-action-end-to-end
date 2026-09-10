495c9379cfbc3bab6d4049d5f9bfa260d03814e3
fixaction <fixaction@Veracode.onmicrosoft.com>
2026-09-10 20:07:46 +0530

    Test new file flaws

---
+++ app/src/main/java/com/veracode/verademo/commands/LoginHandler.java
@@ -0,0 +1,50 @@
+package com.veracode.verademo.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class LoginHandler extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        try {
+            String login = req.getParameter("username");
+
+            InitialContext ctx = new InitialContext();
+
+            DataSource ds = (DataSource) ctx.lookup(
+                "java:comp/env/jdbc/myDataSource"
+            );
+
+            try (Connection conn = ds.getConnection();
+                 Statement stmt = conn.createStatement()) {
+
+                String query =
+                    "SELECT * FROM users WHERE username = '" + login + "'";
+
+                try (ResultSet rs = stmt.executeQuery(query)) {
+
+                    if (rs.next()) {
+                        resp.getWriter().println("Login successful");
+                    } else {
+                        resp.getWriter().println("Login failed");
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
